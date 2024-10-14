ba1831a6e529fdb6ad844d161727a2f7eff7dcd2
yoonjungeun1 <jeyoon.dev@hustation.com>
2024-10-14 14:16:59 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,7 @@
-#!/bin/sh
-# 상위 디렉토리로 이동
 cd ../
 
-# output 디렉토리 생성
-mkdir -p output  # 이미 존재하는 경우에도 에러 없이 넘어감
+mkdir -p output 
 
-# FastAPI 레포지토리 내의 파일을 output으로 복사
-cp -R ./FastAPI/* ./output
+cp -R ./Backend/* ./output
 
-# 복사한 output을 FastAPI 레포지토리로 다시 복사 (필요한 경우)
-cp -R ./output ./FastAPI/
+cp -R ./output ./Backend/
