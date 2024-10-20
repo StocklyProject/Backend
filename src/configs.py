8f52b9858c58d1f2a3ef82b893a13f4845a93b7a
yoonjungeun1 <jeyoon.dev@hustation.com>
2024-10-20 20:39:37 +0900

    Update configs.py

--- src/configs.py
+++ src/configs.py
@@ -4,4 +4,5 @@ import os
 HOST = os.environ.get("MYSQL_HOST")
 USER = os.environ.get("MYSQL_USER")
 PASSWORD = os.environ.get("MYSQL_PASSWORD")
-DATABASE =os.environ.get("stockly")
+DATABASE =os.environ.get("MYSQL_DATABASE")
+REDIS_URL=os.environ.get("REDIS_URL")
