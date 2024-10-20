5f05fb0c6299918e2ad89ce2647d73535a1adc6c
yoonjungeun1 <jeyoon.dev@hustation.com>
2024-10-20 20:40:35 +0900

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI, HTTPException, APIRouter
 from starlette.middleware.cors import CORSMiddleware
-import mysql.connector
-from src.configs import HOST, USER, PASSWORD, DATABASE
+
 from src.user import routes as user_routes
 app = FastAPI()
 router = APIRouter(prefix="/api/v1")
@@ -15,15 +14,6 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# MySQL 데이터베이스에 연결
-def get_db_connection():
-    connection = mysql.connector.connect(
-        host=HOST,
-        user=USER,
-        password=PASSWORD,
-        database=DATABASE
-    )
-    return connection
 @app.get("/")
 def hello():
     return {"message": "메인페이지입니다"}
