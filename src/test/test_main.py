04f101221430dce6562c162e81e817b2878e9312
yoonjungeun1 <jeyoon.dev@hustation.com>
2024-11-01 14:26:24 +0900

    feat : CI 수정

--- src/test/test_main.py
+++
@@ -1,9 +0,0 @@
-from fastapi.testclient import TestClient
-from src.main import app
-
-client = TestClient(app)
-
-def test_read_main():
-    response = client.get("/")
-    assert response.status_code == 200  # 상태 코드가 200인지 확인
-    assert response.json() == {"message": "메인페이지입니다"}  # JSON 응답이 예상값과 일치하는지 확인
