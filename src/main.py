b58cd389d47e480564616cf8ca5110165a351a96
yoonjungeun1 <jeyoon.dev@hustation.com>
2024-11-06 09:35:48 +0900

    Feat: update

--- src/main.py
+++ src/main.py
@@ -37,13 +37,13 @@ async def schedule_websockets():
 # lifespan 핸들러 설정
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    await schedule_mock_websockets()
+    await schedule_websockets()
     logger.info("WebSocket scheduling task executed at app startup.")
 
     # 필요 시 스케줄러를 추가로 사용할 경우
     scheduler = AsyncIOScheduler()
     # scheduler.add_job(schedule_websockets, CronTrigger(hour=10, minute=0))  # 매일 오전 10시 실행
-    scheduler.add_job(schedule_mock_websockets, CronTrigger(minute="*/10"))  # 테스트용 매 분 스케줄링
+    scheduler.add_job(schedule_websockets, CronTrigger(minute="*/10"))  # 테스트용 매 분 스케줄링
     scheduler.start()
 
     try:
