e56e807c9678161ac89ebe936f97b84da2632710
yoonjungeun1 <jeyoon.dev@hustation.com>
2024-11-14 15:30:21 +0900

    feat: send_to_kafka 비동기로 수정

--- src/common/producer.py
+++ src/common/producer.py
@@ -18,16 +18,29 @@ def init_kafka_producer():
         logger.error(f"Error initializing Kafka producer: {e}")
         return None
 
-def send_to_kafka(producer, topic, data):
+# def send_to_kafka(producer, topic, data):
+#     if producer is None:
+#         logger.error("Kafka producer is not initialized.")
+#         return
+#     try:
+#         # 메시지 전송 및 결과 확인
+#         future = producer.send(topic, value=data)
+#         result = future.get(timeout=10)  # 전송 확인
+#         logger.info(f"Sent to Kafka topic {topic}: {data}")
+#         logger.debug(f"Kafka send result: {result}")  # 전송 결과를 로깅
+#         producer.flush()  # 모든 버퍼가 전송될 때까지 대기
+#     except Exception as e:
+#         logger.error(f"Error sending to Kafka: {e}")
+
+# Kafka로 데이터 전송
+async def send_to_kafka(producer, topic, data):
     if producer is None:
         logger.error("Kafka producer is not initialized.")
         return
     try:
-        # 메시지 전송 및 결과 확인
-        future = producer.send(topic, value=data)
-        result = future.get(timeout=10)  # 전송 확인
+        # 메시지를 비동기 전송
+        producer.send(topic, value=data)
         logger.info(f"Sent to Kafka topic {topic}: {data}")
-        logger.debug(f"Kafka send result: {result}")  # 전송 결과를 로깅
-        producer.flush()  # 모든 버퍼가 전송될 때까지 대기
     except Exception as e:
         logger.error(f"Error sending to Kafka: {e}")
+
