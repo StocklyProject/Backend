f70bbcae04758b0176841c30c3f9db94196b540a
yoonjungeun1 <jeyoon.dev@hustation.com>
2024-12-02 11:02:26 +0900

    feat: redis 설정 추가

--- src/database.py
+++ src/database.py
@@ -8,8 +8,18 @@ import time
 
 async def get_redis():
     redis_url = os.getenv("REDIS_URL")
-    redis = await aioredis.from_url(redis_url)
-    return redis
+    if not redis_url:
+        logger.error("REDIS_URL 환경 변수가 설정되지 않았습니다.")
+        raise ValueError("REDIS_URL 환경 변수를 설정하세요.")
+    
+    try:
+        logger.debug(f"Redis 연결 시도: {redis_url}")
+        redis = await aioredis.from_url(redis_url)
+        logger.debug("Redis 연결 성공")
+        return redis
+    except Exception as e:
+        logger.error(f"Redis 연결 실패: {str(e)}")
+        raise
 
 def get_db_connection():
     user = os.getenv("MYSQL_USER")
